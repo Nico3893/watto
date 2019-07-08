49b9455ef1aa5bc3528af80d5880615420bcd751
Nicolas Eberhardt <NicolasEberhardt93@gmail.com>
2019-07-08 18:12:59 +0200

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [ :new, :create ] do
+    resources :items, only: [ :new, :create ]
+    resources :bookings, only: [ :index, :edit, :update ]
+  end
+  resources :items, only: [ :show, :index ] do
+    resources :bookings, only: [ :new, :create ]
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
