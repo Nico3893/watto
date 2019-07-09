2afdca7c1db4b65cfdcd7e48fcddafd92e886656
Nicolas Eberhardt <NicolasEberhardt93@gmail.com>
2019-07-09 15:52:33 +0200

    Validations added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :price, presence: true
+  validates :name, presence: true
+  validates :category, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :bookings
+
+  validates :name, presence: true
 end
